1d05cf991090001eb7ae49b24ecd1b63a38089c6
Henry Nomeland <nomeland@waisman.wisc.edu>
2024-02-27 10:39:29 -0600

    F1 comparisons

--- wave_eval.m
+++ wave_eval.m
@@ -3,9 +3,14 @@
 function wave_eval()
     datavals_test = import_datavals('test_data');
     datavals_base = import_datavals('base_data');
-    fprintf("Median F1 Test: %4.2f \nMedian F1 Base: %4.2f \n", ...
-            median(datavals_test(1).f1, "omitnan"), ...
-            median(datavals_base(1).f1, "omitnan"));
+    for i = 1:size(datavals_test,2)
+        fprintf("Sample %i: \n" + ...
+                "Median F1 Test: %4.2f \n" + ...
+                "Median F1 Base: %4.2f \n", ...
+                i, ...
+                median(datavals_test(i).f1, "omitnan"), ...
+                median(datavals_base(i).f1, "omitnan"));
+    end
 end
 
 % function that imports information from a dataVals matrix
@@ -14,6 +19,7 @@ function datavals = import_datavals(directory)
     try
         datavals = importdata(datavals_path).dataVals;
     catch ME
+        % accounts for the 2 ways dataVals can be stored
         if (strcmp(ME.identifier, 'MATLAB:nonExistentField'))
             datavals = importdata(datavals_path);
         else
