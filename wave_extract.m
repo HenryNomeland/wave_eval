baa7ab2de36f0ae4fb3e833b8184866c1c832bd5
Henry Nomeland <nomeland@waisman.wisc.edu>
2024-04-11 10:44:13 -0500

    additional analysis

--- wave_extract.m
+++ wave_extract.m
@@ -1,10 +1,10 @@
 %%% main function for evaluation
 %%% outputs a csv that is in the correct format for analysis comparisons
-function wave_extract(input_dir, output_dir, output_filename)
+function wave_extract(input_dir, output_dir, output_filename, column_indices)
 
     % import the data and output formant metrics to output dir
     computer = wave_compute();
-    extraction = computer.compute(input_dir);
+    extraction = computer.compute(input_dir, column_indices);
     
     % save files and printout to show that the function is finished
     [~, basename, ~] = fileparts(output_filename);
